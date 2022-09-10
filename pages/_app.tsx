b7fa2a122d3299a22647969e9181063ae01bae54
Kenny Huynh <kenny.huynh@airtable.com>
2022-09-10 16:49:29 -0700

    added auth provider, app providers, react plotly

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import { NavigationBar } from 'components/NavigationBar';
+import { AppProviders } from 'providers';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NavigationBar>
-      <Component {...pageProps} />
-    </NavigationBar>
+    <AppProviders>
+      <NavigationBar>
+        <Component {...pageProps} />
+      </NavigationBar>
+    </AppProviders>
   );
 }
 
