091f38b4de1f37366a66a33d5df0313475590975
Calvin Ho <hoc17@gene.com>
2022-09-10 22:25:39 -0700

    big int monstrosity

--- pages/api/medicare-beneficiary/index.tsx
+++ pages/api/medicare-beneficiary/index.tsx
@@ -1,14 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../../db';
+import { MedicareBeneficiary } from '@prisma/client';
 
-type Data = {
-  name: string;
-};
-
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const { method } = req;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Array<MedicareBeneficiary>>
+) {
+  const {
+    method,
+    query: { state, ageLevel }
+  } = req;
   if (method === 'GET') {
-    //TODO get user based off entered username and password
-  } else {
+    const allMedicareBeneficiaryData = await prisma.medicareBeneficiary.findMany({
+      where: {
+        beneGeoDesc: state as string,
+        beneAgeLvl: ageLevel as string
+      }
+    });
+    res.status(200).json(allMedicareBeneficiaryData);
   }
-  res.status(200).json({ name: 'John Doe' });
 }
--- pages/api/opioid-prescription-rate/index.tsx
+++ pages/api/opioid-prescription-rate/index.tsx
@@ -1,14 +1,75 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { prisma } from '../../../db';
+import { OpioidPrescriptionRate } from '@prisma/client';
 
-type Data = {
-  name: string;
+type ModifiedOpioidPrescriptionRate = {
+  opioidPrescriptionRateId: number;
+  year: string;
+  prscrbrGeoLvl: string;
+  prscrbrGeoDesc: string;
+  totPrscrbrs: string;
+  totOpioidPrscrbrs: string;
+  totOpioidClms: string;
+  totClms: string;
+  opioidPrscrbngRate: number;
+  opioidPrscrbngRate5yChg: number;
+  opioidPrscrbngRate1yChg: number;
+  laTotOpioidClms: number;
+  laOpioidPrscrbngRate: number;
+  laOpioidPrscrbngRate5yChg: number;
+  laOpioidPrscrbngRate1yChg: number;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  const { method } = req;
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Array<ModifiedOpioidPrescriptionRate>>
+) {
+  const {
+    method,
+    query: { state }
+  } = req;
   if (method === 'GET') {
-    //TODO get user based off entered username and password
-  } else {
+    const allOpioidPrescriptionData = await prisma.opioidPrescriptionRate.findMany({
+      where: { prscrbrGeoDesc: state as string }
+    });
+    const modifedOpioidPrescriptionData: Array<ModifiedOpioidPrescriptionRate> =
+      allOpioidPrescriptionData.map((opioidDataPoint) => {
+        const {
+          opioidPrescriptionRateId,
+          year,
+          prscrbrGeoLvl,
+          prscrbrGeoDesc,
+          totPrscrbrs,
+          totOpioidPrscrbrs,
+          totOpioidClms,
+          totClms,
+          opioidPrscrbngRate,
+          opioidPrscrbngRate5yChg,
+          opioidPrscrbngRate1yChg,
+          laTotOpioidClms,
+          laOpioidPrscrbngRate,
+          laOpioidPrscrbngRate5yChg,
+          laOpioidPrscrbngRate1yChg
+        } = opioidDataPoint;
+        return {
+          opioidPrescriptionRateId,
+          year,
+          prscrbrGeoLvl,
+          prscrbrGeoDesc,
+          totPrscrbrs: totPrscrbrs.toString(),
+          totOpioidPrscrbrs: totOpioidPrscrbrs.toString(),
+          totOpioidClms: totOpioidClms.toString(),
+          totClms: totClms.toString(),
+          opioidPrscrbngRate,
+          opioidPrscrbngRate5yChg,
+          opioidPrscrbngRate1yChg,
+          laTotOpioidClms,
+          laOpioidPrscrbngRate,
+          laOpioidPrscrbngRate5yChg,
+          laOpioidPrscrbngRate1yChg
+        };
+      });
+
+    res.status(200).json(modifedOpioidPrescriptionData);
   }
-  res.status(200).json({ name: 'John Doe' });
 }
